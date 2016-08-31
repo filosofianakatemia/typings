4f23b51d22accbb57f097285f7f0480a6fa8c1b8
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-08-31 20:55:14 +0300

    Use del instead of reserved word delete for HTTP DELETE, as it seems to be exported as well by koa-route.

--- koa-route/index.d.ts
+++ koa-route/index.d.ts
@@ -4,6 +4,6 @@ declare module 'koa-route' {
   export function get(path: string, handler: (c: Context, ...args: string[]) => void): (c: Context, next: Function) => any
   export function post(path: string, handler: (c: Context, ...args: string[]) => void): (c: Context, next: Function) => any
   export function put(path: string, handler: (c: Context, ...args: string[]) => void): (c: Context, next: Function) => any
-  export function delete(path: string, handler: (c: Context, ...args: string[]) => void): (c: Context, next: Function) => any
+  export function del(path: string, handler: (c: Context, ...args: string[]) => void): (c: Context, next: Function) => any
 
 }
